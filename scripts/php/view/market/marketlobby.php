e3f013b67c9f29b176f4dc8688ec67a908440153
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-03-22 17:38:45 -0300

    starting development of the view marketlobby

--- scripts/php/view/market/marketlobby.php
+++ scripts/php/view/market/marketlobby.php
@@ -1,10 +1,26 @@
 <?php
     error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
+    require_once '../../controller/marketcontroller.php';
+    require_once '../../controller/usercontroller.php';
+    require_once '../../controller/genericcontroller.php';
+
     session_start();
-    echo $_SESSION['pk_id_user'];
+    /*echo $_SESSION['pk_id_user'];
     echo "<br>";
-    echo $_SESSION['pk_id_market'];
+    echo $_SESSION['pk_id_market'];*/
+
+    if(isset($_SESSION['pk_id_user']) && isset($_SESSION['pk_id_market'])){
+        $columns = "*";
+        $tables = "employer";
+        $whereClause = "pk_id_user = " .  $_SESSION['pk_id_user'] . " and " . "pk_id_market = " . $_SESSION['pk_id_market'];
+
+        $additionalEmployerData = GenericController::select($columns, $tables, $whereClause);
+
+        if(!empty($additionalEmployerData)){
+            //insane logics
+        }
+    }
 ?>
 <!--
 <!DOCTYPE html>
