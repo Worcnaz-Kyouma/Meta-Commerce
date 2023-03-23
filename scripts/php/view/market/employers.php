033594299034dc9279329d9aa545b5f209cc3b87
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-03-23 17:30:51 -0300

    starting implementations of the view employers and some table test

--- scripts/php/view/market/employers.php
+++ scripts/php/view/market/employers.php
@@ -1,3 +1,31 @@
 <?php
+if(isset($_SESSION['pk_id_user']) && isset($_SESSION['pk_id_market'])){
+    $user = getSessionedUser($_SESSION['pk_id_user']);
+    standardValidateForMarket($user);
 
+    $market = getSessionedMarket($_SESSION['pk_id_market']);
+    standardValidateForMarket($market);
+
+    $additionalEmployerData = getEmployerData($_SESSION['pk_id_user'], $_SESSION['pk_id_market']);
+    standardValidateForMarket($additionalEmployerData);
+
+    if($additionalEmployerData->ds_role != "Boss"){
+        header('Location: marketlobby.php');
+        die();
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employers</title>
+    <link rel="stylesheet" href="../../../../styles/style.css">
+</head>
+<body>
+    
+</body>
+</html>
